1011aa0d53bdb7d501a46cd753558120421848c6
Onur Akkaya <onur.akkaya@d-teknoloji.com.tr>
2019-11-14 17:20:59 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceProcess;
+
+namespace DSM.Agents.TrackerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ServiceBase.Run(new TrackerService());
+        }
+    }
+}
---
+++ TrackerService.cs
@@ -0,0 +1,20 @@
+﻿using DSM.Controller.Tracker.Shared;
+
+namespace DSM.Agents.TrackerService
+{
+    public class TrackerService : DSMAgent
+    {
+        public TrackerService() : base("DSM.Agents.TrackerService") { }
+
+        public override void ActionMain(object authObj)
+        {
+            string apiKey = (string)authObj;
+
+            TrackerEngine wcfTracker = new TrackerEngine(apiKey, TrackerType.Endpoint);
+            wcfTracker.PostTrackerValues();
+
+            TrackerEngine connectionStringTracker = new TrackerEngine(apiKey, TrackerType.ConnectionString);
+            connectionStringTracker.PostTrackerValues();
+        }
+    }
+}
